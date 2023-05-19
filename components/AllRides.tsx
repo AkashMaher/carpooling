3b619ff8f9800809af6890b296f0543419718d41
Akash <aakashrajp5@gmail.com>
2023-05-19 23:04:04 +0530

    fix

--- components/AllRides.tsx
+++ components/AllRides.tsx
@@ -46,7 +46,7 @@ const ActivityItem: FC<{
   },[userLat, userLong])
 
   async function calculateRoute(destination:any) {
-    console.log(userLat, userLong)
+    console.log(userLat && userLong?'checking rides':'check')
     if (destination === "" || !userLat || !userLong) {
       return;
     }
@@ -63,7 +63,7 @@ const ActivityItem: FC<{
 
   calculateRoute(activity?.from)
 
-console.log(distanceFrom)
+console.log(distanceFrom<=Maxdistance?true:false)
   const isTo = activity?.driver !== '0x0000000000000000000000000000000000000000'
   // const isTx = activity?.transaction_hash
   const isTx = undefined
